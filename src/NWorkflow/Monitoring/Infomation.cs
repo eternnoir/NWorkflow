915ed0e74b8bbca7cbd354e658001a54463ccbba
eternnoir <eternnoir@gmail.com>
2015-06-22 17:47:49 +0800

    Add monitor need class.

---
+++ src/NWorkflow/Monitoring/Infomation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NWorkflow.Monitoring
+{
+    public class Infomation
+    {
+        private IFlow flow;
+        private IJob job;
+        private MonitorLevel level;
+        private string message;
+    }
+}
